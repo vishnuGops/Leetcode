f88fc5bae33df0910ce21c4cf9635191ecbb64e9
Vishnu Gopal Rajan <vishvargops@gmail.com>
2023-10-25 20:07:51 -0700

    add binary strings

---
+++ 67.add-binary.java
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=67 lang=java
+ *
+ * [67] Add Binary
+ */
+
+// @lc code=start
+class Solution {
+    public String addBinary(String a, String b) {
+        int carry = 0;
+        StringBuilder result = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            if (i >= 0) {
+                sum += a.charAt(i) - '0';
+                i--;
+            }
+            if (j >= 0) {
+                sum += b.charAt(j) - '0';
+                j--;
+            }
+            result.append(sum % 2);
+            carry = sum / 2;
+        }
+
+        if (carry > 0) {
+            result.append(carry);
+        }
+        return result.reverse().toString();
+    }
+}
+// @lc code=end

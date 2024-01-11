ef749edf38e0a66e2cc2adbd8859ccaed2763454
Vishnu Gopal Rajan <35251589+vishnuGops@users.noreply.github.com>
2024-01-10 16:36:33 -0800

    contains duplicate

---
+++ 219.contains-duplicate-ii.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=219 lang=java
+ *
+ * [219] Contains Duplicate II
+ */
+
+// @lc code=start
+
+import java.util.Map;
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i]) && i - map.get(nums[i]) <= k) {
+                return true;
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+
+    }
+}
+// @lc code=end

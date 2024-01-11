96ca7e72328614c1a52acc92c81ec3f01860e269
Vishnu Gopal Rajan <35251589+vishnuGops@users.noreply.github.com>
2024-01-11 11:36:19 -0800

    83

---
+++ 83.remove-duplicates-from-sorted-list.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=83 lang=java
+ *
+ * [83] Remove Duplicates from Sorted List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return head;
+    }
+}
+// @lc code=end

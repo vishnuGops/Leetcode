116bfecebd3fe29e98fb73487ac34d376efc3e1f
Vishnu Gopal Rajan <35251589+vishnuGops@users.noreply.github.com>
2024-02-16 12:13:59 -0800

    412

---
+++ 412.fizz-buzz.java
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=412 lang=java
+ *
+ * [412] Fizz Buzz
+ */
+
+// @lc code=start
+
+import java.util.ArrayList;
+
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        ArrayList<String> answer = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                answer.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                answer.add("Fizz");
+            } else if (i % 5 == 0) {
+                answer.add("Buzz");
+            } else {
+                answer.add(Integer.toString(i));
+            }
+        }
+        return answer;
+    }
+}
+// @lc code=end
